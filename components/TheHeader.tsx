4fc67a2068ca5a46981a169304ad42da663a8e19
Abdullo Abdumamadov <abdullo_a@hotmail.com>
2023-05-06 14:16:28 +0500

    header was created

---
+++ components/TheHeader.tsx
@@ -0,0 +1,13 @@
+import { Header, Navbar, Text } from "@mantine/core";
+
+
+const TheHeader = () => {
+    return (
+        <Header height={5}>
+            <Navbar>
+                <Text>Home</Text>
+            </Navbar>
+        </Header>
+    );
+}
+export default TheHeader;
